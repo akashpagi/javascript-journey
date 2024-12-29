4508a0cc004c9362574ad5244828e703e0938c16
akashpagi <88099663+akashpagi@users.noreply.github.com>
2024-12-29 11:41:21 +0530

    Boolean in JS

---
+++ JAVASCRIPT_BASIC/Boolean/v62_Boolean.js
@@ -0,0 +1,11 @@
+console.log(' -- Boolean --');
+
+
+// Using Boolean Literal
+
+let isLoggedIn = true;
+let isAdmin = false;
+console.log(isLoggedIn); // Output: true
+console.log(isAdmin);    // Output: false
+
+
