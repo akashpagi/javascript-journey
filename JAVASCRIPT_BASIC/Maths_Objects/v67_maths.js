f8aa95c11ea47ed48209cf0cf0ad2d5e41155dac
akashpagi <88099663+akashpagi@users.noreply.github.com>
2025-01-04 19:43:33 +0530

    Maths object in JS

---
+++ JAVASCRIPT_BASIC/Maths_Objects/v67_maths.js
@@ -0,0 +1,14 @@
+console.log('-- Maths --')
+
+console.log(Math.floor(4.10)); // output : 4
+console.log(Math.floor(-4.7)); // Output: -5
+console.log(Math.floor(-4.3)); // Output: -5
+
+
+console.log(Math.round(4.10)); // Output: 4
+console.log(Math.round(4.5));  // Output: 5
+console.log(Math.round(4.6));  // Output: 5
+console.log(Math.round(-4.4)); // Output: -4
+console.log(Math.round(-4.7)); // Output: -5
+console.log(Math.round(-4.3)); // Output: -4
+
